be1a48a102a1fe6bfbd5d24a48d16a1914c8d42d
SkyLandYT2 <83461291+SkyLandYT2@users.noreply.github.com>
2024-08-13 13:59:40 +0300

    console,js.
    console commands,navigator.js update

--- Scripts/navigator.js
+++ Scripts/navigator.js
@@ -22,7 +22,7 @@ document.write(`
             left: 22.8%;
             width: 1232px;
             height: 100%;
-	    z-index: -1;
+            z-index: -1;
         }
         hr {
             height: 50px;
@@ -42,7 +42,7 @@ document.write(`
             background-color: black;
             border-radius: 10px;
             border: 2px solid #333;
-	    width: 600px;
+            width: 600px;
         }
         h2 {
             text-align: center;
@@ -112,14 +112,14 @@ document.write(`
             pointer-events: none;
             transition: opacity 0.3s ease-in-out;
         }
-            .image-zoom.active {
-                opacity: 1;
-                pointer-events: auto;
-            }
-            .image-zoom img {
-                max-width: 90%;
-                max-height: 90%;
-            }
+        .image-zoom.active {
+            opacity: 1;
+            pointer-events: auto;
+        }
+        .image-zoom img {
+            max-width: 90%;
+            max-height: 90%;
+        }
         header {
             background-color: #333;
             border: 2px solid #000;
@@ -140,19 +140,19 @@ document.write(`
             margin-bottom: 10px;
             margin-top: 10px;
         }
-            nav li a {
-                display: block;
-                font-family: Raleway;
-                border: 2px solid #000;
-                border-radius: 5px;
-                color: white;
-                text-align: center;
-                padding: 10px;
-                text-decoration: none;
-            }
-                nav li a:hover {
-                    background-color: #111;
-                }
+        nav li a {
+            display: block;
+            font-family: Raleway;
+            border: 2px solid #000;
+            border-radius: 5px;
+            color: white;
+            text-align: center;
+            padding: 10px;
+            text-decoration: none;
+        }
+        nav li a:hover {
+            background-color: #111;
+        }
         .generated-links {
             position: absolute;
             left: 100%;
@@ -174,11 +174,14 @@ document.write(`
             text-decoration: none;
             background-color: #222;
         }
+        .generated-links a:hover {
+            background-color: darkorchid;
+        }
         table {
             border-collapse: collapse;
             width: 100%;
             border: 2px solid white;
-            padding: 1px
+            padding: 1px;
         }
         th, td {
             color: white;
@@ -201,33 +204,59 @@ document.write(`
             background-color: black;
             border: 2px solid white;
         }
-        #promoTab {
-            position: absolute;
+        #promoTabsContainer {
+            position: fixed;
             bottom: 0;
             left: 0;
+            display: flex;
+            flex-direction: column;
+            gap: 5px;
+            z-index: 1000;
+        }
+        #promoTab1, #promoTab2 {
             background-color: #007bff;
             color: #fff;
-            padding: 10px 15px;
+            padding: 7px 10px;
             border-radius: 5px;
             cursor: pointer;
-            z-index: 1000;
-            font-size: 20px;
+            font-size: 15px;
+            position: relative;
+            z-index: 1;
+        }
+        #promoTab2 {
+            z-index: 2; /* Ensures promoTab2 is above promoTab1 */
         }
         #socialLinks {
             display: none;
-            flex-direction: column;
+            flex-direction: row;
             align-items: center;
             gap: 10px;
-            font-size: 10px;
+            font-size: 13px;
+            position: absolute;
+            top: 50%;
+            left: 100%;
+            transform: translateY(-50%);
+            background-color: rgba(0, 0, 0, 0.8);
+            padding: 10px;
+            border-radius: 10px;
+            border: 2px solid #0c33ee;
         }
-        #promoTab:hover #socialLinks {
+        #promoTab1:hover #socialLinks,
+        #promoTab2:hover #socialLinks {
             display: flex;
-            background-color: #0c33ee;
             color: darkorchid;
         }
         #socialLinks a {
+            font-family: 'Roboto', sans-serif;
             text-decoration: none;
             color: #fff;
+            background-color: #0c33ee;
+            padding: 3px 10px;
+            border-radius: 5px;
+            font-size: 14px;
+        }
+        #socialLinks a:hover {
+            background-color: darkorchid;
         }
         .search-container {
             display: inline-block;
@@ -253,10 +282,10 @@ document.write(`
             border-bottom: 1px solid white;
         }
         .search-container:hover {
-            width: 220px
+            width: 220px;
         }
         .search-container:hover .search-bar {
-            width: 150px; 
+            width: 150px;
         }
         #searchResults {
             position: absolute;
@@ -271,10 +300,10 @@ document.write(`
             display: none;
             padding: 10px;
         }
-            #searchResults p {
-                margin: 5px 0;
-                width: 600px;
-            }
+        #searchResults p {
+            margin: 5px 0;
+            width: 600px;
+        }
         .search-container:focus-within #searchResults {
             display: block;
         }
@@ -288,20 +317,19 @@ document.write(`
     <header>
         <nav>
             <ul>
+                <li class="tab"><a href="../index.html">Home</a></li>
+                <li class="tab"><a href="../Objects/objectswiki.html">Objects</a></li>
+                <li class="tab"><a href="../Entities/entitieswiki.html">Entities</a></li>
                 <li class="tab">
-                    <a href="../index.html">Home</a>
-                </li>
-                <li class="tab">
-                    <a href="../Objects/objectswiki.html">Objects</a>
-                </li>
-                <li class="tab">
-                    <a href="../Entities/entitieswiki.html">Entities</a>
+                    <a>Promotabs</a>
+                    <ul class="generated-links">
+                        <li><a href="../Promotabs/promotab1.html">Promo Tab 1</a></li>
+                        <li><a href="../Promotabs/promotab2.html">Promo Tab 2</a></li>
+                    </ul>
                 </li>
                 <li class="tab">
                     <a>Worlds</a>
                     <ul class="generated-links">
-                        <li><a href="../Worlds/testlevelswiki.html">Test Levels</a></li>
-                        <li><a href="../Worlds/anomalyworldswiki.html">Anomaly Worlds</a></li>
                         <li><a href="../Worlds/otherworldswiki.html">Other Worlds</a></li>
                     </ul>
                 </li>
@@ -325,24 +353,12 @@ document.write(`
                         <li><a href="../Events/333ameventswiki.html">3:33 AM Events</a></li>
                     </ul>
                 </li>
-                <li class="tab">
-                    <a href="../Structures/structureswiki.html">Structures</a>
-                </li>
-                <li class="tab">
-                    <a href="../ConsoleCommands/consolecommandswiki.html">Console Commands</a>
-                </li>
-                <li class="tab">
-                    <a href="../Dreams/dreamswiki.html">Dreams</a>
-                </li>
-                <li class="tab">
-                    <a href="../Achievements/achievementswiki.html">Achievements</a>
-                </li>
-                <li class="tab">
-                    <a href="../GameEndings/endingswiki.html">Game Endings</a>
-                </li>
-                <li class="tab">
-                    <a href="../InteractiveMap/interactablemap.html">Map of Everything</a>
-                </li>
+                <li class="tab"><a href="../Structures/structureswiki.html">Structures</a></li>
+                <li class="tab"><a href="../ConsoleCommands/consolecommandswiki.html">Console Commands</a></li>
+                <li class="tab"><a href="../Dreams/dreamswiki.html">Dreams</a></li>
+                <li class="tab"><a href="../Achievements/achievementswiki.html">Achievements</a></li>
+                <li class="tab"><a href="../GameEndings/endingswiki.html">Game Endings</a></li>
+                <li class="tab"><a href="../InteractiveMap/interactablemap.html">Map of Everything</a></li>
                 <li class="tab">
                     <a style="border: 1px solid #333; border-radius: 5px;" onmouseover="this.style.backgroundColor='#333';" onmouseout="this.style.backgroundColor='transparent';"></a>
                 </li>
@@ -359,22 +375,33 @@ document.write(`
                     <a style="border: 1px solid #333; border-radius: 5px;" onmouseover="this.style.backgroundColor='#333';" onmouseout="this.style.backgroundColor='transparent';"></a>
                 </li>
                 <li class="tab">
-                    <a style="color:red; font-size: 17px;" onmouseover="this.style.backgroundColor='#333';" onmouseout="this.style.backgroundColor='transparent';"><b>Last Updated: 0.7.0</b></a>
+                    <a style="color:red; font-size: 14px;" onmouseover="this.style.backgroundColor='#333';" onmouseout="this.style.backgroundColor='transparent';"><b>Last Updated: 0.8.0 WIP</b></a>
                 </li>
             </ul>
-            <div id="promoTab">
-                <div id="socialLinks">
-                    <a href="https://github.com/SquareZeb" target="_blank">Github</a>
-                    <a href="https://www.youtube.com/channel/UCSPSGIg3wWMowzCL-1tc_Hg" target="_blank">Youtube</a>
-                    <a href="https://discord.gg/vzkMs8sH2z" target="_blank">Discord</a>
-                </div>
-                <img src="../Images/Promo.png" alt="Social Icon" width="30" height="30" style="border-radius: 50%">
-                Created by: <span style="color: lawngreen;">Square</span><span style="color: aqua;">Zeb</span>
-            </div>
         </nav>
     </header>
+    <div id="promoTabsContainer">
+        <div id="promoTab2">
+            <div id="socialLinks">
+                <a href="https://github.com/SquareZeb" target="_blank">Github</a>
+                <a href="https://www.youtube.com/channel/UCSPSGIg3wWMowzCL-1tc_Hg" target="_blank">Youtube</a>
+                <a href="https://discord.gg/vzkMs8sH2z" target="_blank">Discord</a>
+            </div>
+             <span style="color: lawngreen; font-family: 'Roboto', sans-serif;"><span style="color: white;">Created by:</span> Square</span><span style="color: aqua;">Zeb</span>
+
+        </div>
+        <div id="promoTab1">
+            <div id="socialLinks">
+                <a href="https://github.com/SkyLandYT2" target="_blank">Github</a>
+                <a href="https://www.youtube.com/@SkyLandYT2" target="_blank">Youtube</a>
+                <a href="https://discord.gg/88bAD62dvY" target="_blank">Discord</a>
+            </div>
+            <span style="font-family: 'Roboto', sans-serif;">And by: SkyLandYT2</span>
+        </div>
+    </div>
 </body>
 </html>
+
 `);
 
 
--- Scripts/warning.js
+++ Scripts/warning.js
@@ -21,7 +21,7 @@ document.write(`
     <h3 style="text-align:center; font-style:oblique;">Credits:</h3>
     <p style="text-align:center;color:red;">Note: This game has not been fully released and there may be secrets and easter eggs that have not been discovered which may render this archive outdated.</p>
     <p style="text-align:center;color:lawngreen;"><a href="https://mrdrnose.itch.io/votv">Voices of the Void:</a></p>
-    <p style="text-align:center;color:lawngreen;">If any new secret or hidden easter egg pops up you may contact me through <a href="https://discord.com/invite/uDhdXRATpP">here:</a></p>
+    <p style="text-align:center;color:lawngreen;">If any new secret or hidden easter egg pops up you may contact me through <a href="https://discord.gg/JA95KNQtHA">here:</a></p>
     <div class="image-zoom" onclick="closeImage()">
         <img id="zoomed-img" src="" alt="Zoomed Image">
     </div>
